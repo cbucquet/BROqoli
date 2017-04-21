edba505b99e0d1f38dbfdbabb16acead279e3f62
bucquet <bucquet@macholmeshome.fios-router.home>
2017-04-21 13:31:35 -0400

    Initial Commit

---
+++ Crypto-Currencies/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Crypto-Currencies
+//
+//  Created by bucquet on 4/21/17.
+//  Copyright © 2017 bucquet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
